03ef91b066f670776d706c182b727341bcf8199f
Tsetsenbileg <tsetsenbileg0413@gmail.com>
2022-02-09 10:45:34 +0800

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "antd/dist/antd.css";
+import "../styles/style.min.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
